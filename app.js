c766e924f50f9d6a1ccded8d0ce583629e8b9193
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-04-30 11:49:43 +1000

    Create navbar using express.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
+// setup static middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// add index.html to public folder
+// use SSR
+// });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+  res.status(404).send("resource not found");
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+  console.log("server is listening on port 5000...");
 });
