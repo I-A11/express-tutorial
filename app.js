d6f034c763d7836f35abc1af5cb3b0d2efb0813a
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-02 11:36:14 +1000

    Use different types of middleware.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const logger = require("./logger");
+const authorize = require("./authorize");
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+// app.use([logger, authorize]);
+// app.use(express.static("./public"));
+app.use(morgan("tiny"));
 
-app.get("/", logger, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About page");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products page");
+});
+
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items page");
 });
 
 app.listen(5000, () => {
